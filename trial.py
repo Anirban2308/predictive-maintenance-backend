56c7d10e054db0efcbffabd3cf3b4fcec7f8596a
Anirban Dey <anirbandey8032@gmail.com>
2026-02-15 10:48:42 +0530

    Fix CORS for Vercel frontend

--- trial.py
+++ trial.py
@@ -19,9 +19,12 @@ os.environ["OPENAI_API_KEY"] = "dummy"
 # FASTAPI INITIALIZATION
 # ------------------------------------------------
 app = FastAPI()
+origins = [
+    "https://predictive-maintenance-frontend.vercel.app"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
